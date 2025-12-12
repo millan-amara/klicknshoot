303687ed107d80173c661bbb6b0dcde030c20d82
Hughes <onyangohughes8611@gmail.com>
2025-12-12 03:24:30 +0300

    deploy frontend to vercel

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.PROD ? '/api' : 'http://localhost:5000/api',
+
+  baseURL: import.meta.env.PROD ? 'https://klicknshoot.onrender.com/api' : 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json',
   },
--- client/vite.config.js
+++ client/vite.config.js
@@ -84,7 +84,8 @@ export default defineConfig({
     // port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        // target: 'http://localhost:5000',
+        target: 'https://klicknshoot.onrender.com',
         changeOrigin: true,
       }
     }
